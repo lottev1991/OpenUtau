4bc46bfcec09ba2f41ec1582bd38640e4649516c
Lotte V <61566058+lottev1991@users.noreply.github.com>
2025-11-27 01:30:58 +0100

    Revert "Reapply "Merge pull request #43 from lottev1991/Superbox2147/master""
    This reverts commit c0ace709f75bbbfd4fc6ea7323ee998822518289.

--- OpenUtau.Core/Util/Onnx.cs
+++ OpenUtau.Core/Util/Onnx.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.IO;
-using System.Runtime.InteropServices;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.ML.OnnxRuntime;
 using OpenUtau.Core.Util;
-using Serilog;
 
 namespace OpenUtau.Core {
     public class GpuInfo {
@@ -18,9 +15,6 @@ namespace OpenUtau.Core {
     }
 
     public class Onnx {
-
-        private static bool cudaAvailable = OS.IsLinux() && CudaGpuDetector.IsCudaAvailable() && CudaGpuDetector.IsCuDnnAvailable();
-
         private static readonly Dictionary<int, OrtEpDevice> devices = initializeDevices();
 
         private static Dictionary<int, OrtEpDevice> initializeDevices() {
@@ -44,11 +38,6 @@ namespace OpenUtau.Core {
                 "CPU",
                 "CoreML"
                 };
-            } else if (cudaAvailable) {
-                return new List<string> {
-                "CPU",
-                "CUDA"
-                };
             } else if (OS.IsAndroid()) {
                 return new List<string> {
                 "CPU",
@@ -56,19 +45,14 @@ namespace OpenUtau.Core {
                 };
             }
             return new List<string> {
-                "CPU"        
+                "CPU"
             };
         }
 
         public static List<GpuInfo> getGpuInfo() {
-            if (cudaAvailable) {
-                return CudaGpuDetector.GetCudaDevices();
-            }         
-     
             if (OS.IsAndroid()) {
                 return new List<GpuInfo>();
             }
-
             List<GpuInfo> gpuList = new List<GpuInfo>();
             var env = OrtEnv.Instance();
             var ortDevices = env.GetEpDevices();
@@ -89,6 +73,11 @@ namespace OpenUtau.Core {
                 gpuList.Add(new GpuInfo {
                     deviceId = i++,
                     description = description
+                });
+            }
+            if (gpuList.Count == 0) {
+                gpuList.Add(new GpuInfo {
+                    deviceId = 0,
                 });
             }
             return gpuList;
@@ -115,9 +104,6 @@ namespace OpenUtau.Core {
                     break;
                 case "CoreML":
                     options.AppendExecutionProvider_CoreML(CoreMLFlags.COREML_FLAG_ENABLE_ON_SUBGRAPH);
-                    break;
-                case "CUDA":
-                    options.AppendExecutionProvider_CUDA(Preferences.Default.OnnxGpu);
                     break;
                 case "NNAPI":
                     options.AppendExecutionProvider_Nnapi();
