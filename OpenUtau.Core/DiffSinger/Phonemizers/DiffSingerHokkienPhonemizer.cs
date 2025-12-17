c404553cb4e85761239ae21a2c6a3985234f4059
Lotte V <61566058+lottev1991@users.noreply.github.com>
2025-12-18 00:14:57 +0100

    test

---
+++ OpenUtau.Core/DiffSinger/Phonemizers/DiffSingerHokkienPhonemizer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OpenUtau.Api;
+using Pinyin;
+
+namespace OpenUtau.Core.DiffSinger {
+    [Phonemizer("DiffSinger Hokkien Phonemizer", "DIFFS NAN", language: "NAN")]
+    public class DiffSingerHokkienPhonemizer : DiffSingerBasePhonemizer {
+        protected override string GetDictionaryName()=>"dsdict-nan.yaml";
+        public override string GetLangCode()=>"nan";
+        protected override string[] Romanize(IEnumerable<string> lyrics) {
+            return Pinyin.TaiLo.Instance.HanziToPinyin(lyrics.ToList(), MinTone.Style.NORMAL, Pinyin.Error.Default).Select(res => res.pinyin).ToArray();
+        }
+    }
+}
